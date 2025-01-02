cfda86fa1e7ffb6812016e772764fb00fd7d0554
jnakum23 <tinunakum98@gmail.com>
2025-01-02 08:49:21 +0530

    Add files via upload

---
+++ 2652.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int xyz,i,sum=0;
+	xyz=i%10;
+	sum=sum+xyz*xyz*xyz;
+	xyz=i%10;
+	xyz=xyz/10;
+	sum=sum+xyz*xyz*xyz;
+	xyz=i%100;
+	{
+	ifelse(i=sum);
+	printf("%d number is aramstrong");
+	sum=0;
+	}
+}
